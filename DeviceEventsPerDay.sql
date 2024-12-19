e8c749623f19efe03e683766788a3b1753c4f781
Cotterilla12 <117513254+Cotterilla12@users.noreply.github.com>
2024-12-19 21:05:36 +0000

    Update DeviceEventsPerDay.sql

--- DeviceEventsPerDay.sql
+++ DeviceEventsPerDay.sql
@@ -3,5 +3,6 @@ SELECT
     DAYOFWEEK(StartOfExposure) AS EventDay,
     COUNT(*) AS EventCount
 FROM irradiationeventdimtable
+    WHERE StartOfExposure BETWEEN '2024-01-01' AND '2024-12-31'
 GROUP BY StationName, DAYOFWEEK(StartOfExposure)
 ORDER BY StationName
