e07d0691b74b893e92c815c542f305be0f34b174
Cotterilla12 <117513254+Cotterilla12@users.noreply.github.com>
2024-12-19 21:01:54 +0000

    Update DeviceEventCount.sql

--- DeviceEventCount.sql
+++ DeviceEventCount.sql
@@ -1,4 +1,5 @@
 SELECT StationName, COUNT(*) AS EventCount
 FROM irradiationeventdimtable
+  WHERE StartOfExposure BETWEEN '2024-01-01' AND '2024-12-31'
 GROUP BY StationName
 ORDER BY EventCount DESC;
